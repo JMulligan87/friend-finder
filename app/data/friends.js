6f086eff1677e92bca95d706da426250d3af1bcd
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-08-31 13:24:59 -0400

    finished home.html

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [{
+    name: "Derick",
+    photo: "https://i.kym-cdn.com/entries/icons/original/000/016/546/hidethepainharold.jpg",
+    scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}
+]
+
+module.exports = friends;
