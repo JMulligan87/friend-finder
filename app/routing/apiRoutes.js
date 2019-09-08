fc70b858b078a458d6cd6e734c0236e9c79e2a45
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-09-07 21:36:52 -0400

    updated apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,30 @@
 var friendsData = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
+        var userInput = req.body.scores
+        var scoresArr = [];
+        var bestFriend = 0;
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
 
+            for (var j = 0; j < userInput.length; j++) {
+                diff += Math.abs(friends[i].scores - userInput[j])
+            }
+            scoresArr.push(diff);
+        }
+        for (var i = 0; i < scoresArr.length; i++) {
+            if (scoresArr[i] <= scoresArr[bestFriend]) {
+                bestFriend = i
+            }
+        }
+        var bestestFriend = friends[bestFriend]
+        res.json(bestestFriend)
+
+        friends.push(req.body)
     });
 };
